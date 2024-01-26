82906b7b2e9fc1c83baa62c4386c5090345678c4
John Olayemi <olayemi.johnayo@gmail.com>
2024-01-26 16:52:25 +0100

    feat - implement xunit test with moq

---
+++ movie.test/SearchServiceTest.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using movie.application.DTO.Response;
+using movie.application.MovieApiIntegration.Interface;
+using movie.application.Services.Implementation;
+using movie.application.Services.Interface;
+
+namespace movie.test
+{
+    public class SearchServiceTest
+    {
+        private readonly Mock<IMovieClient> _mockMovieClient;
+        private readonly Mock<IMemoryCache> _mockMemoryCache;
+        private readonly Mock<ICacheHandler> _mockCacheHandler;
+        private readonly SearchService _sut;
+
+        public SearchServiceTest()
+        {
+            _mockMovieClient = new Mock<IMovieClient>();
+            _mockMemoryCache = new Mock<IMemoryCache>();
+            _mockCacheHandler = new Mock<ICacheHandler>();
+            _sut = new SearchService(_mockMovieClient.Object, _mockMemoryCache.Object, _mockCacheHandler.Object);
+        }
+
+        [Fact]
+        public async Task SearchMovies_DoesNotReturnNull()
+        {
+            // Arrange
+            _mockMovieClient.Setup(s => s.GetMovies("", 1)).ReturnsAsync((new SearchMovieResponseDTO {Search = new List<Search> { new Search {Title="star wars", imdbID = "tt002345", Poster = "www.thumbnail.com", Type = "movie", Year = "2009" } }  }, 200));
+
+            _mockCacheHandler.Setup(s => s.SaveSearchQuery(""));
+
+            // Act
+            var result = await _sut.SearchMovies("", 1);
+
+            // Assert
+            Assert.NotNull(result);
+         
+        }
+
+        [Fact]
+        public async Task SearchMovies_ReturnsListOfMovies()
+        {
+            // Arrange
+            _mockMovieClient.Setup(s => s.GetMovies("", 1)).ReturnsAsync((new SearchMovieResponseDTO { Search = new List<Search> { new Search { Title = "star wars", imdbID = "tt002345", Poster = "www.thumbnail.com", Type = "movie", Year = "2009" } } }, 200));
+
+            _mockCacheHandler.Setup(s => s.SaveSearchQuery(""));
+
+            // Act
+            var result = await _sut.SearchMovies("", 1);
+
+            // Assert
+            Assert.IsType<ApiResponseDTO<List<GetMoviesResponseDTO>>>(result.Item1);
+
+        }
+    }
+}
